b84390ae099b34789501398941a6250090783b38
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-10 19:02:26 +0200

    api

---
+++ src/services/api/auth-api.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://connections-api.herokuapp.com',
+});
+
+// Utility to add JWT
+const setToken = token => {
+  if (token) {
+    return (instance.defaults.headers.common[
+      'Authorization'
+    ] = `Bearer ${token}`);
+  }
+  instance.defaults.headers.common['Authorization'] = '';
+};
+
+/* POST @ /users/signup */
+export const signUpUser = async data => {
+  const { data: result } = await instance.post('/users/signup', data);
+  setToken(result.token);
+  return result;
+};
+
+/* POST @ /users/login */
+export const logInUser = async data => {
+  const { data: result } = await instance.post('/users/login', data);
+  setToken(result.token);
+  return result;
+};
+
+// * GET @ /users/current
+export const currentUser = async token => {
+  try {
+    setToken(token);
+    const { data } = await instance.get('/users/current');
+    return data;
+  } catch (error) {
+    setToken();
+    throw error;
+  }
+};
+
+export const logOut = async () => {
+  const { data } = instance.post('/users/logout');
+  setToken();
+  console.log(data);
+  return data;
+};
+
+export default instance;
---
+++ src/services/api/contacts-api.js
@@ -0,0 +1,18 @@
+import instance from './auth-api';
+
+export const getAllContacts = async () => {
+  const { data } = await instance.get('/contacts');
+  return data;
+};
+
+export const addContact = async data => {
+  const { data: result } = await instance.post('/contacts', data);
+  console.log(result);
+  return result;
+};
+
+export const deleteContact = async id => {
+  const { data } = await instance.delete(`/contacts/${id}`);
+  console.log(data);
+  return data;
+};
