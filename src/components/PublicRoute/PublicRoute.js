ae750845d04c86193cd369b27d0d59a8006250e3
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-11 01:09:03 +0200

    Update PublicRoute.js

--- src/components/PublicRoute/PublicRoute.js
+++ src/components/PublicRoute/PublicRoute.js
@@ -1,11 +1,18 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { selectIsUserLogged } from 'redux/auth/auth-selector';
+import {
+  selectIsUserLogged,
+  selectIsUserToken,
+} from 'redux/auth/auth-selector';
 
 const PublicRoute = () => {
   const isLogin = useSelector(selectIsUserLogged);
+  const isToken = useSelector(selectIsUserToken);
 
+  if (!isLogin && isToken) {
+    return <p>...loading</p>;
+  }
   if (isLogin) {
     return <Navigate to="/contacts" />;
   }
