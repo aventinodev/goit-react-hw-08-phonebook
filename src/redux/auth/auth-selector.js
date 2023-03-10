4dbdf0372c477c9eda104e923f6cc3ff702c4527
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-11 01:07:41 +0200

    Update auth-selector.js

--- src/redux/auth/auth-selector.js
+++ src/redux/auth/auth-selector.js
@@ -1,6 +1,8 @@
 export const selectIsUserLogged = store => store.auth.isLogged;
+export const selectIsUserToken = store => store.auth.token;
 export const selectGetUser = store => store.auth.user;
 export const selectGetAuth = store => {
   const { isLogged, token } = store.auth;
+
   return { isLogged, token };
 };
