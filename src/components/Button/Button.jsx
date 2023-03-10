e2b72932a6186c9faa2d012c5e68410d03174b50
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-10 18:59:59 +0200

    create Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import css from './Button.module.css';
+
+const Button = ({ children, type = 'submit' }) => {
+  return (
+    <button className={css.btn} type={type}>
+      {children}
+    </button>
+  );
+};
+export default Button;
