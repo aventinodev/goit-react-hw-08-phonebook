4b6cf060b47429d5ee0022473451babfa15db953
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-10 19:10:02 +0200

    Create root-reducer.js

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,23 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import contactsReducer from './contacts/contacts-slice';
+import filterReducer from './filter/filter-slice';
+import authReducer from './auth/auth-slice';
+
+const persistConfig = {
+  key: 'token',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+  auth: persistedAuthReducer,
+});
+
+export default rootReducer;
