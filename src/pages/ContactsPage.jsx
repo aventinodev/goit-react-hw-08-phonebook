cb632720e695e599cc4c208950ac78ff7a140728
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-11 23:49:13 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -3,6 +3,7 @@ import ContactsList from 'components/ContactsList/ContactsList';
 import FormContact from 'components/FormContact/FormContact';
 import Filter from 'components/Filter/Filter';
 import Title from 'components/Title/Title';
+
 const ContactsPage = () => {
   return (
     <>
