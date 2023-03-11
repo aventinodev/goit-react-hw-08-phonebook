79ee2a36292637e5a008ab307d5ac8f7b6647a19
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-11 15:03:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
 
-import { ToastContainer } from 'react-toastify';
+import { lazy, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+
 import { currentUser } from 'redux/auth/auth-operations';
-import PublicRoute from './PublicRoute/PublicRoute';
-import PrivateRoute from './PublicRoute/PublicRoute';
+import RestrictedRoute from './RestrictedRoute';
+import useAuth from 'hooks/useAuth';
+import PrivateRoute from './RestrictedRoute';
 import { Loader } from 'components/Loader/Loader';
 import '../index.css';
 
-import AppBar from './AppBar/AppBar';
-import AppFooter from './AppFooter/AppFooter';
+// import AppBar from './AppBar/AppBar';
+// import AppFooter from './AppFooter/AppFooter';
 
 const HomePage = lazy(() => import('pages/HomePage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage'));
@@ -20,36 +21,44 @@ const LoginPage = lazy(() => import('pages/LoginPage'));
 const ContactsPage = lazy(() => import('pages/ContactsPage'));
 const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
-// import { Wrapper, Title } from './App.styled';
-
 const App = () => {
   const dispatch = useDispatch();
+  const { isCurrent } = useAuth();
+
   useEffect(() => {
     dispatch(currentUser());
   }, [dispatch]);
 
-  return (
-    <>
-      <AppBar />
-      {/* <AuthLayout> */}
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route element={<PublicRoute />}>
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path="/contacts" element={<ContactsPage />} />
-          </Route>
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-      {/* </AuthLayout> */}
-      <AppFooter />
-      <ToastContainer autoClose={1000} />
-    </>
+  return isCurrent ? (
+    <Loader />
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
 
