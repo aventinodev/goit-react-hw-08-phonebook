4047dc8bb0aa3b1436f075c9cb1b4f002f505860
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-12 12:16:42 +0200

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -7,9 +7,9 @@ import useAuth from 'hooks/useAuth';
  */
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const { isLoggedIn, isRefreshing } = useAuth();
+  const { isLoggedIn } = useAuth();
 
-  const shouldRedirect = !isLoggedIn && !isRefreshing;
+  const shouldRedirect = !isLoggedIn;
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
