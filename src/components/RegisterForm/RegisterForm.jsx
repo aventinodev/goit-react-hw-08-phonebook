f1154a1c4fdffb1bcce89ecefe6d991d88646102
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-18 18:33:05 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import useForm from 'hooks/useForm';
 import initialState from './initialState';
+
 import TextField from 'components/TextField/TextField';
 import fields from './fields';
 import Button from 'components/Button/Button';
 import css from './RegisterForm.module.css';
 
 const RegisterForm = ({ onSubmit }) => {
-  const [state, setState] = useState({ ...initialState });
+  const { state, handleChange, handleSubmit } = useForm({
+    initialState,
+    onSubmit,
+  });
   const { name, email, password } = state;
 
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    setState(prevState => {
-      return { ...prevState, [name]: value };
-    });
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(state);
-    setState({ ...initialState });
-  };
+  // const [state, setState] = useState({ ...initialState });
+  // const { name, email, password } = state;
+
+  // const handleChange = ({ target }) => {
+  //   const { name, value } = target;
+  //   setState(prevState => {
+  //     return { ...prevState, [name]: value };
+  //   });
+  // };
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   onSubmit(state);
+  //    };
   return (
     <form className={css.form} onSubmit={handleSubmit}>
       <TextField value={name} handleChange={handleChange} {...fields.name} />
