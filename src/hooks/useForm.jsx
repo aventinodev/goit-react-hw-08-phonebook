ba1eb5dec5a7dfa6c438c9036dfc849cdefbf198
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-18 18:33:49 +0200

    Update useForm.jsx

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,7 +1,12 @@
 import { useState, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { selectAuthError } from 'redux/auth/auth-selector';
+import { selectError } from 'redux/contacts/contacts-selectors';
 
 const useForm = ({ initialState, onSubmit }) => {
   const [state, setState] = useState({ ...initialState });
+
+  const errorAuth = useSelector(selectAuthError);
 
   const handleChange = useCallback(
     ({ target }) => {
@@ -16,10 +21,14 @@ const useForm = ({ initialState, onSubmit }) => {
   const handleSubmit = e => {
     e.preventDefault();
     onSubmit({ ...state });
-
-    // setState({ ...initialState });
+    resetForm();
   };
 
+  const resetForm = () => {
+    if (!errorAuth) {
+      setState({ ...initialState });
+    }
+  };
   return { state, setState, handleChange, handleSubmit };
 };
 
