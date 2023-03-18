551c7b1fa7e0a07e5f9114369ff4e3bdbec2327f
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-18 18:35:04 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,26 +1,18 @@
-import { ColorRing } from 'react-loader-spinner';
+import ClipLoader from 'react-spinners/ClipLoader';
 
-export const Loader = () => {
+export const Loader = ({ size = 40, color = '#0984b9' }) => {
   return (
-    // <div
-    //   style={{
-    //     height: 60,
-    //     marginTop: 30,
-    //     marginBottom: 30,
-    //   }}
-    // >
-    <ColorRing
-      visible={true}
-      height="60"
-      width="60"
-      ariaLabel="blocks-loading"
-      wrapperStyle={{
-        display: 'block',
-        margin: '0 auto',
-      }}
-      wrapperClass="blocks-wrapper"
-      colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-    />
-    // </div>
+    <div
+      style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}
+    >
+      <ClipLoader
+        display="block"
+        margin="0 auto"
+        color={color}
+        size={size}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
   );
 };
