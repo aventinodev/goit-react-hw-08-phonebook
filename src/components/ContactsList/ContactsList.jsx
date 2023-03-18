1f63bf0d8cd0ee7e40fdc02520da55674642a889
Olena Bondar <olena.bondar1060@gmail.com>
2023-03-18 18:35:22 +0200

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -3,14 +3,21 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/contacts-operations';
-import { selectFilteredContacts } from 'redux/contacts/contacts-selectors';
+import {
+  selectFilteredContacts,
+  selectError,
+  selectOperation,
+} from 'redux/contacts/contacts-selectors';
 
+import { Loader } from 'components/Loader/Loader';
 import ContactItem from 'components/ContactItem/ContactItem';
 import css from './ContactsList.module.css';
 
 export const ContactsList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectFilteredContacts);
+  const operation = useSelector(selectOperation);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -18,8 +25,7 @@ export const ContactsList = () => {
 
   return (
     <>
-      {/* {isLoading && !error && <Loader />} */}
-      {/* {error && <p className={css.notes}>Something wrong!</p>} */}
+      {operation === 'fetch' && !error && <Loader />}
 
       <ul className={css.list}>
         {contacts.map(({ id, name, number }) => (
